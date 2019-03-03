62245830d9cdd3a4923c5bf7920bdf46ab6321f2
16cmc21@queensu.ca <16cmc21@queensu.ca>
2019-03-03 15:15:42 -0500

    cnn that concatenates mlp and image cnn

---
+++ project/mixed_inputs.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 14:34:04 2019
+
+@author: Colin Cumming
+"""
+from pyimagesearch import attributeProcessing
+from pyimagesearch import models
+from sklearn.model_selection import train_test_split
+from keras.layers.core import Dense
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.layers import concatenate
+import numpy as np
+import argparse
+import locale
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", type=str, required=True,
+	help="path to input dataset of house images")
+args = vars(ap.parse_args())
+
+print("[INFO] loading house attributes...")
+inputPath = os.path.sep.join([args["dataset"], "name of file"])
+df = attributeProcessing.get_house_attributes(inputPath)
+ 
+# load the house images and then scale the pixel intensities to the
+# range [0, 1]
+print("[INFO] loading house images...")
+images = attributeProcessing.load_house_images(df, args["dataset"])
+images = images / 255.0
+
+print("[INFO] processing data...")
+split = train_test_split(df, images, test_size=0.25, random_state=42)
+(trainAttrX, testAttrX, trainImagesX, testImagesX) = split
+
+maxPrice = trainAttrX["price"].max()
+trainY = trainAttrX["price"] / maxPrice
+testY = testAttrX["price"] / maxPrice
+
+(trainAttrX, testAttrX) = attributeProcessing.process_house_attributes(df,
+	trainAttrX, testAttrX)
+
+mlp = models.create_mlp(trainAttrX.shape[1], regress=False)
+cnn = models.create_cnn(64, 64, 3, regress=False)
+
+combinedInput = concatenate([mlp.output, cnn.output])
+
+x = Dense(4, activation="relu")(combinedInput)
+x = Dense(1, activation="linear")(x)
+
+model = Model(inputs=[mlp.input, cnn.input], outputs=x)
+
+model = models.create_cnn(32, 64, 3, regress=True)
+opt = Adam(lr=1e-3, decay=1e-3 / 200)
+model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
+ 
+# train the model
+print("[INFO] training model...")
+model.fit(trainImagesX, trainY, validation_data=(testImagesX, testY),
+	epochs=200, batch_size=8)
+
+# make predictions on the testing data
+print("[INFO] predicting house prices...")
+preds = model.predict(testImagesX)
+ 
+# compute the difference between the *predicted* house prices and the
+# *actual* house prices, then compute the percentage difference and
+# the absolute percentage difference
+diff = preds.flatten() - testY
+percentDiff = (diff / testY) * 100
+absPercentDiff = np.abs(percentDiff)
+ 
+# compute the mean and standard deviation of the absolute percentage
+# difference
+mean = np.mean(absPercentDiff)
+std = np.std(absPercentDiff)
+ 
+# finally, show some statistics on our model
+locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+print("[INFO] avg. house price: {}, std house price: {}".format(
+	locale.currency(df["price"].mean(), grouping=True),
+	locale.currency(df["price"].std(), grouping=True)))
+print("[INFO] mean: {:.2f}%, std: {:.2f}%".format(mean, std))
